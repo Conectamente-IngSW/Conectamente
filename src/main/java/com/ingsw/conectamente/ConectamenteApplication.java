d99396c9969645692ec91044c729f47c1967a069
dreelliot <dreelliot@hotmail.com>
2025-05-10 18:13:30 -0500

    Setup inicial Conectamente

---
+++ src/main/java/com/ingsw/conectamente/ConectamenteApplication.java
@@ -0,0 +1,13 @@
+package com.ingsw.conectamente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectamenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConectamenteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingsw/conectamente/ConectamenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsw.conectamente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectamenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
