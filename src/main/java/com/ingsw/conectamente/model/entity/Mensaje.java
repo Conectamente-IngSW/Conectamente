fff6ce38ffbe302c98711a3bd887f3aed7628707
NicoleBances <nicolebances929@gmail.com>
2025-05-14 21:19:11 -0500

    Implementacion del entity mensaje

---
+++ src/main/java/com/ingsw/conectamente/model/entity/Mensaje.java
@@ -0,0 +1,27 @@
+package com.ingsw.conectamente.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "Mensaje")
+public class Mensaje {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idMensaje;
+
+    @Column(columnDefinition = "TEXT")
+    private String contenido;
+
+    private LocalDateTime fechaEnvio;
+
+    //FK
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario",
+            foreignKey = @ForeignKey(name = "Usuario_idUsuario" ))
+    private Usuario Usuario_idUsuario;
+}
