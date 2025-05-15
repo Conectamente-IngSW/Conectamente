14ea3f26be6e01cea48d9395466f47b5240c8d44
NicoleBances <nicolebances929@gmail.com>
2025-05-14 21:51:02 -0500

    Implementacion model.entity Calificacion y CalificacionRepository

---
+++ src/main/java/com/ingsw/conectamente/repository/CalificacionRepository.java
@@ -0,0 +1,7 @@
+package com.ingsw.conectamente.repository;
+
+import com.ingsw.conectamente.model.entity.Calificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalificacionRepository extends JpaRepository<Calificacion, Integer> {
+}
