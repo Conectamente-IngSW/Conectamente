cc51235d59c7d2c9fa89f804b8d0aba1b0bf2e50
NicoleBances <nicolebances929@gmail.com>
2025-07-03 12:25:04 -0500

    Implementacion de cors en el web security

--- src/main/java/com/ingsw/conectamente/config/WebSecurityConfig.java
+++ src/main/java/com/ingsw/conectamente/config/WebSecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -77,4 +82,19 @@ public class WebSecurityConfig {
         //TODO: Propociona el AuthenticationManager que gestionara la autenticacion basada en los detalles de usuario y contrasena
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // OJO: usa setAllowedOrigins, no addAllowedOrigin
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
